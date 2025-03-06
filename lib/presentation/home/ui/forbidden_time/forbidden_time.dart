71c42633e5b194168f6ccb0aa426c85324eb812d
Ird-Irfan <ird.maxcode@gmail.com>
2025-03-06 09:38:40 +0600

    Remove Quick Tools section and update font configuration

--- lib/presentation/home/ui/forbidden_time/forbidden_time.dart
+++ lib/presentation/home/ui/forbidden_time/forbidden_time.dart
@@ -14,7 +14,6 @@ class _ForbiddenTimeState extends State<ForbiddenTime>
     with SingleTickerProviderStateMixin {
   bool isExpanded = false;
   late AnimationController _animationController;
-  late Animation<double> _heightAnimation;
   late Animation<double> _fadeAnimation;
 
   @override
@@ -23,9 +22,6 @@ class _ForbiddenTimeState extends State<ForbiddenTime>
     _animationController = AnimationController(
       duration: const Duration(milliseconds: 500),
       vsync: this,
-    );
-    _heightAnimation = Tween<double>(begin: 90, end: 300).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
     );
     _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
       CurvedAnimation(parent: _animationController, curve: Curves.easeInOut),
