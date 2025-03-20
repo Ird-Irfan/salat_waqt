e0074bf868b78a21d75bd9153ee55a7dd023dc6e
Ird-Irfan <ird.maxcode@gmail.com>
2025-03-20 07:25:31 +0600

    UI উপাদানগুলির অ্যানিমেশন এবং স্থান পরিবর্তন করা হয়েছে
    - CurrentPrayerTime, ForbiddenTime, AnimatedCard, এবং AnimatedLangCard ক্লাসে AnimatedContainer এর পরিবর্তে AnimatedSize ব্যবহার করা হয়েছে, যা অ্যানিমেশন সময় বৃদ্ধি করে এবং UI এর অভিজ্ঞতা উন্নত করে।
    - UI উপাদানগুলির মধ্যে স্থান এবং প্যাডিং উন্নত করা হয়েছে, যা সামগ্রিক ডিজাইনকে আরও পরিষ্কার এবং ব্যবহারকারী বান্ধব করে তুলেছে।
    - AnimatedSize এর জন্য নতুন অ্যানিমেশন সময় এবং কার্ভ যুক্ত করা হয়েছে, যা অ্যানিমেশনকে আরও মসৃণ করে।

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,32 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import awesome_notifications
+import connectivity_plus
+import geolocator_apple
+import network_info_plus
+import package_info_plus
+import path_provider_foundation
+import share_plus
+import shared_preferences_foundation
+import sqflite_darwin
+import sqlite3_flutter_libs
+import url_launcher_macos
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
+  Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
+}
